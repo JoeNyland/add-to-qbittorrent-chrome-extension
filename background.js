b999de1fedc8fe281c49024e04bb3a4bc49c6587
Joe Nyland <7666402+JoeNyland@users.noreply.github.com>
2026-06-10 23:21:20 +0100

    Revert "Change menu text"
    This reverts commit 06d06ad4a3755b48b20b55ba666846d2562f7be3.

--- background.js
+++ background.js
@@ -32,7 +32,7 @@ function createContextMenus() {
     chrome.contextMenus.create({
       id: "add:__none",
       parentId: "addNormal",
-      title: "Add",
+      title: "Add (no category)",
       contexts: ["link", "selection"],
       targetUrlPatterns: ["*://*/*.torrent", "magnet:*"],
     });
@@ -40,7 +40,7 @@ function createContextMenus() {
     chrome.contextMenus.create({
       id: "add_paused:__none",
       parentId: "addPaused",
-      title: "Add",
+      title: "Add (no category)",
       contexts: ["link", "selection"],
       targetUrlPatterns: ["*://*/*.torrent", "magnet:*"],
     });
